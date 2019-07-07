3b7780aa2267dff6ead8b03474a51b09f18ddba5
ande zeng <zengande@outlook.com>
2019-07-07 23:50:54 +0800

    添加 IconFont

---
+++ src/Mobile/Together/Together.UI/Components/Icon.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Together.UI.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Icon : Label
+    {
+        public Icon()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private BindableProperty TypeProperty = BindableProperty.Create(nameof(Type), typeof(string), typeof(Icon), "\u0030");
+        public string Type
+        {
+            get => (string)GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+    }
+}
