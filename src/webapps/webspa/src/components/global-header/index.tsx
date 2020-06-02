25af93d52a343544c935baf961568b0d728d34cf
zengande <zengande@outlook.com>
2020-06-02 17:02:52 +0800

    添加相关页面

---
+++ src/webapps/webspa/src/components/global-header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GlobalHeader extends React.PureComponent {
+    render() {
+        return (
+            <div>Header</div>
+        )
+    }
+}
+
+export default GlobalHeader;
---
+++ src/webapps/webspa/src/pages/activities/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+class CreatePage extends React.PureComponent {
+    render() {
+        return (
+            <Button>Create</Button>
+        )
+    }
+}
+
+export default CreatePage;
