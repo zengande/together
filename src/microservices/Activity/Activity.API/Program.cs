10b1585d05ed86834b7384509e71e4364a6ed25c
zengande <zengande@outlook.com>
2020-06-05 20:47:50 +0800

    添加 migrate database

---
+++ src/microservices/Activity/Activity.API/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Together.Activity.Infrastructure.Data;
+using Together.BuildingBlocks.Infrastructure.Data;
+
+namespace Activity.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build()
+                .MigrateDatabase<ActivityDbContext>()
+                .Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
