a21e20a9b13414c472d79d5668722cb1ed90771e
zengande <zengande@outlook.com>
2020-06-30 15:16:03 +0800

    update webspa config

--- src/webapps/webspa/config.ts
+++ src/webapps/webspa/config.ts
@@ -1,8 +1,9 @@
 const env = process.env.NODE_ENV
+console.log(env)
 
 export default {
     // 当前应用地址
-    AppBaseAddress: env === "production" ? "https://together2.fun" : "http://localhost:3000",
+    AppBaseAddress: env === "development" ? "http://localhost:3000" : "https://together2.fun",
     // API 地址
-    ApiBaseAddress: env === "production" ? "https://api.together2.fun" : "http://localhost:3000/api"
+    ApiBaseAddress: env === "development" ? "http://localhost:3000/api" : "https://api.together2.fun"
 }
